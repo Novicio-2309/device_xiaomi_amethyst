c92fc6d9c58215d19277ee22ebcbc29008dcc986
Novicio+MillenniumTeam <157868622+Novicio-2309@users.noreply.github.com>
2026-09-05 13:14:47 +0800

    Rename product from lineage_amethyst to halcyon_amethyst

--- halcyon_amethyst.mk
+++ halcyon_amethyst.mk
@@ -8,12 +8,12 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit_only.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
 # Inherit some common Lineage stuff.
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+$(call inherit-product, vendor/halcyon/config/common_full_phone.mk)
 
 # Inherit from amethyst device
 $(call inherit-product, device/xiaomi/amethyst/device.mk)
 
-PRODUCT_NAME := lineage_amethyst
+PRODUCT_NAME := halcyon_amethyst
 PRODUCT_DEVICE := amethyst
 PRODUCT_MANUFACTURER := Xiaomi
 PRODUCT_BRAND := Redmi
