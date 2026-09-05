3bdb8ec459ac60a07ec133734d9cb20fec507d96
Novicio+MillenniumTeam <157868622+Novicio-2309@users.noreply.github.com>
2026-09-05 13:29:06 +0800

    Change common configuration inheritance in makefile

--- halcyon_amethyst.mk
+++ halcyon_amethyst.mk
@@ -8,7 +8,7 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit_only.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
 # Inherit some common Lineage stuff.
-$(call inherit-product, vendor/halcyon/config/common_full_phone.mk)
+$(call inherit-product, vendor/halcyon/config/common.mk)
 
 # Inherit from amethyst device
 $(call inherit-product, device/xiaomi/amethyst/device.mk)
