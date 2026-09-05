1ecea5d8338e565d0226410d8f11148aaa18ce2d
Novicio+MillenniumTeam <157868622+Novicio-2309@users.noreply.github.com>
2026-09-05 13:16:19 +0800

    Remove overlay configuration from device.mk
    Removed overlay configuration from device.mk

--- device.mk
+++ device.mk
@@ -291,10 +291,6 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     NcmTetheringOverlay
 
-# Overlay
-DEVICE_PACKAGE_OVERLAYS += \
-    $(LOCAL_PATH)/derp-overlay
-
 # Partitions
 PRODUCT_USE_DYNAMIC_PARTITIONS := true
 
